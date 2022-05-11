57aab5cad7df69dd058b31dc2ad0077b96a6e08c
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2022-05-10 23:49:40 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -20,9 +20,43 @@ public class Exercise2 {
 		}
 	}
 
+	
 	public Integer getMenorNumeroSaltos(int a []) {
+		//seleccionar el mas cercano a 50
+		int cont=0;
+		int pos=0;
+		int pos2=0;
+		int pos3=0;
+		int mayor=a[0];
+		
+		while(true) {//repita el proceso
+			
+			for (int i = 0; i < a.length; i++) {
+				//posicion1
+				if(a[i]<=50 && a[i] > mayor) {
+					pos=i;
+					cont++;
+				break;
 
-		// TO DO
-		return -1;
-	}
+				}
+				
+				if((a[i]<= 50+a[pos])&&a[i]>a[pos]) {
+					mayor=a[i];
+					pos2=i;
+					cont++;
+					
+				}
+			
+				
+			
+	
+			}
+			
+			System.out.println("la posicion  es: "+pos);
+			System.out.println(a[pos]);
+			System.out.println("la poscicon 2 es: "+mayor);
+			System.out.println("la siguientes es: "+pos3);
+			System.out.println(a[pos3]);
+			System.out.println("total de saltos : "+cont);
+return cont;}} }
 }
