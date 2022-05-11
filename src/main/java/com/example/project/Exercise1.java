edfe73798f9f094ba326be76ff8bf0566810c70b
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2022-05-10 22:57:04 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -14,9 +14,42 @@ public class Exercise1 {
 		}
 	}
 
+
 	public String convertirBaseGabriel(int a) {
 
-		// TO DO
-		return "El numero proporcionado no esta en base Gabriel.";
+		    String numCadena= Integer.toString(a);
+		    int[] numero= new int[numCadena.length()];
+		    int numerodos=0;
+		    int numeroGab=0;
+		    String numeroGabriel="";
+		    int k=numCadena.length();
+		    //rellenar el arreglo
+		    for (int i = 0; i < numCadena.length();i++) {
+		    	numero[i]=Integer.parseInt(numCadena.substring(i,i+1));
+
+
+		    //parametros
+		    		if(numero[i]<3 && numero[i]>=0 && numerodos<=1 && numero[numero.length-1]!=2 && numCadena.indexOf("0")>=0&& numCadena.indexOf("1")>=0) {//solo numeros de 0 a 2
+		    	
+		    			if(numero[i]==2) {
+		    				numerodos++;//para que la cantidad de "2" no sea mayor A 1
+		    			}
+		    			//convertir a base gabriel
+		    
+		    			int ga=(int) Math.pow(2,k);
+
+		    			int	numeroGabR= numero[i]*(ga-1);
+		    			numeroGab+=numeroGabR;
+		    		}else {
+
+		    			return "El numero proporcionado no esta en base Gabriel.";
+		    		}
+		    
+		    		k--;//el indice va disminuyendo
+
+		    }
+
+		    numeroGabriel= Integer.toString(numeroGab);
+			return numeroGabriel;	
 	}
 }
